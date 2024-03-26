da516df9aaa4bbc5067201862bbd6add81c9e703
brianpocasangre <brianpocasangre1@gmail.com>
2024-03-26 12:35:39 -0700

    add bootstrap and components folder with ListGroups.tsx

---
+++ firstreactapp/src/components/ListGroup.tsx
@@ -0,0 +1,15 @@
+import { useLayoutEffect } from 'react';
+
+function ListGroup() {
+  return (
+    <ul className='list-group'>
+      <li className='list-group-item'>An Item</li>
+      <li className='list-group-item'>A Second Item</li>
+      <li className='list-group-item'>A Third Item</li>
+      <li className='list-group-item'>A Fourth Item</li>
+      <li className='list-group-item'>and A Fifth Item</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
