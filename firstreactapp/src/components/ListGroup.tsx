06f9ebb6f0155369a4990ae5d4bd02392df37855
brianpocasangre <brianpocasangre1@gmail.com>
2024-03-26 13:43:50 -0700

    go over event handlers and hook functions

--- firstreactapp/src/components/ListGroup.tsx
+++ firstreactapp/src/components/ListGroup.tsx
@@ -1,14 +1,39 @@
-import { useLayoutEffect } from 'react';
+//import { Fragment } from 'react/jsx-runtime'; and add <Fragment> before and </Fragement> to close
+// you can replace a div with fragment to not over use div
 
+import { useState } from 'react';
+
+// import { MouseEvent } from 'react';
 function ListGroup() {
+  let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
+  //Hook function that allowas us to tap in to built in features in react
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  //event handler
+  //   const handleClick = (event: MouseEvent) => console.log(event);
   return (
-    <ul className='list-group'>
-      <li className='list-group-item'>An Item</li>
-      <li className='list-group-item'>A Second Item</li>
-      <li className='list-group-item'>A Third Item</li>
-      <li className='list-group-item'>A Fourth Item</li>
-      <li className='list-group-item'>and A Fifth Item</li>
-    </ul>
+    // same as Fragment
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No Items Found</p>}
+      <ul className='list-group'>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? 'list-group-item active'
+                : 'list-group-item'
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
